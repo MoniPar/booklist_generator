a13a7fd049dc45271b20946462731b0ae045c948
Monique Parnis <monique.parnis@gmail.com>
2022-10-16 14:24:22 +0000

    Connect to APIs and confirm access to Google Sheet

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#23456789 123456789 123456789 1233456789 123456789 123456789 123456789 1234567890
+import gspread 
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("book_list")
+
+price_list = SHEET.worksheet("subject-book-price_list")
+
+data = price_list.get_all_values()
+
+print(data)
