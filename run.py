1f05e94b2dfcb0bc9c6c3e7f4e5b70994ff88214
Monique Parnis <monique.parnis@gmail.com>
2022-10-18 19:49:00 +0000

    Create get_student_name function to get student name from user

--- run.py
+++ run.py
@@ -1,20 +1,35 @@
-#23456789 123456789 123456789 1233456789 123456789 123456789 123456789 1234567890
 import gspread 
 from google.oauth2.service_account import Credentials
 
+# Define the scope
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
 
+# Add credentials to the account and authorise the client sheet
 CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# Get the spreadsheet
 SHEET = GSPREAD_CLIENT.open("book_list")
 
-price_list = SHEET.worksheet("subject-book-price_list")
 
-data = price_list.get_all_values()
+def get_student_name():
+    """
+    Get student's first and last name from the user
+    """
+    print("Welcome to BookList Generator!\n")
+    print("In order to run this program efficiently, "
+          "please enter the correct student information "
+          "when prompted and press the 'Enter' key.\n")
+    print("When entering the student's first name, "
+          "include their second name, if available. " 
+          "Example: Mary Jane\n")
+    name = input("Enter student's first name: \n")
+    surname = input("Enter student's last name: \n")
+    print(f"\nThank you! You are compiling a book list for {name} {surname}")
+    
 
-print(data)
+get_student_name()
